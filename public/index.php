70c6a03f7abd8340933f9ec6332ea5a44d319ea5
Mark Skelton <markskelton10@gmail.com>
2024-10-27 13:06:49 +0000

    Create index.php HTML boilerplate

--- public/index.php
+++ public/index.php
@@ -1,2 +1,14 @@
 <?php
-echo 'Hello World!';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP Crud App</title>
+</head>
+<body>
+  <h1>Hello World!</h1>
+</body>
+</html>
