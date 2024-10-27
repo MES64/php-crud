86d65cd2c0eec5a1110e95597501247c58a69e76
Mark Skelton <markskelton10@gmail.com>
2024-10-27 13:12:25 +0000

    Set title and header using PHP variable

--- public/index.php
+++ public/index.php
@@ -1,14 +1,14 @@
 <?php
-
+$title = "Simple PHP CRUD -- Contacts";
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>PHP Crud App</title>
+  <title><?php echo $title ?></title>
 </head>
 <body>
-  <h1>Hello World!</h1>
+  <h1><?php echo $title ?></h1>
 </body>
 </html>
