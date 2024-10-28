2298b106d3bc5986351f167210dbe66ab322935c
Mark Skelton <markskelton10@gmail.com>
2024-10-28 18:43:41 +0000

    Create add contact functionality
    Also fixed Database connection bug.

--- app/Database.php
+++ app/Database.php
@@ -1,15 +1,15 @@
 <?php
 namespace App;
 
-include_once "conn_vars.php";
-
 class Database
 {
   private $dbconn;
 
   public function __construct()
   {
-    $this->dbconn = mysqli_connect($host, $username, $password, $dbname);
+    include "conn_vars.php";
+    
+    $this->dbconn = mysqli_connect($my_host, $my_username, $my_password, $my_dbname);
   }
 
   public function getContacts()
@@ -18,5 +18,11 @@ class Database
     $result = mysqli_query($this->dbconn, $sql);
     $contacts = mysqli_fetch_all($result, MYSQLI_ASSOC);
     return $contacts;
+  }
+
+  public function addContact($name_first, $name_last, $email, $birth_date)
+  {
+    $sql = "INSERT INTO contacts (name_first, name_last, email, birth_date) VALUES ('$name_first', '$name_last', '$email', '$birth_date');";
+    mysqli_query($this->dbconn, $sql);
   }
 }
--- public/add_edit.php
+++ public/add_edit.php
@@ -1,5 +1,22 @@
 <?php
+require_once "../app/Database.php";
+
+use App\Database;
+
+$database = new Database();
+
 $title = "Simple PHP CRUD -- Add / Edit Contacts";
+
+if ($_POST) {
+  $name_first = $_POST['name_first'];
+  $name_last = $_POST['name_last'];
+  $email = $_POST['email'];
+  $birth_date = $_POST['birth_date'];
+
+  $database->addContact($name_first, $name_last, $email, $birth_date);
+
+  header("Location: index.php");
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,7 +27,7 @@ $title = "Simple PHP CRUD -- Add / Edit Contacts";
 </head>
 <body>
   <h1><?php echo $title ?></h1>
-  <form action="">
+  <form method="POST">
     <div>
       <label for="name_first">First Name</label>
       <input type="text" id="name_first" name="name_first" placeholder="First Name">
