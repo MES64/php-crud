e2cdfacdb8bd24d29e6978a68ee7b236254c0d2d
Mark Skelton <markskelton10@gmail.com>
2024-10-28 20:12:06 +0000

    Create edit functionality

--- app/Database.php
+++ app/Database.php
@@ -8,21 +8,34 @@ class Database
   public function __construct()
   {
     include "conn_vars.php";
-    
+
     $this->dbconn = mysqli_connect($my_host, $my_username, $my_password, $my_dbname);
   }
 
   public function getContacts()
   {
-    $sql = "SELECT * FROM contacts";
+    $sql = "SELECT * FROM contacts;";
     $result = mysqli_query($this->dbconn, $sql);
     $contacts = mysqli_fetch_all($result, MYSQLI_ASSOC);
     return $contacts;
   }
 
+  public function getContact($id)
+  {
+    $sql = "SELECT * FROM contacts WHERE id = $id;";
+    $result = mysqli_query($this->dbconn, $sql);
+    $contact = mysqli_fetch_assoc($result);
+    return $contact;
+  }
+
   public function addContact($name_first, $name_last, $email, $birth_date)
   {
     $sql = "INSERT INTO contacts (name_first, name_last, email, birth_date) VALUES ('$name_first', '$name_last', '$email', '$birth_date');";
+    mysqli_query($this->dbconn, $sql);
+  }
+
+  public function updateContact($id, $name_first, $name_last, $email, $birth_date) {
+    $sql = "UPDATE contacts SET name_first = '$name_first', name_last = '$name_last', email = '$email', birth_date = '$birth_date' WHERE id = $id;";
     mysqli_query($this->dbconn, $sql);
   }
 }
--- public/add_edit.php
+++ public/add_edit.php
@@ -8,14 +8,26 @@ $database = new Database();
 $title = "Simple PHP CRUD -- Add / Edit Contacts";
 
 if ($_POST) {
+  $id = $_POST['id'] ?? null;
   $name_first = $_POST['name_first'];
   $name_last = $_POST['name_last'];
   $email = $_POST['email'];
   $birth_date = $_POST['birth_date'];
 
-  $database->addContact($name_first, $name_last, $email, $birth_date);
+  if ($id) {
+    $database->updateContact($id, $name_first, $name_last, $email, $birth_date);
+  } else {
+    $database->addContact($name_first, $name_last, $email, $birth_date);
+  }
 
   header("Location: index.php");
+}
+
+$id = $_GET['id'] ?? null;
+if ($id) {
+  $contact = $database->getContact($id);
+} else {
+  $contact = [];
 }
 ?>
 <!DOCTYPE html>
@@ -30,20 +42,25 @@ if ($_POST) {
   <form method="POST">
     <div>
       <label for="name_first">First Name</label>
-      <input type="text" id="name_first" name="name_first" placeholder="First Name">
+      <input type="text" id="name_first" name="name_first" placeholder="First Name"
+      value="<?php echo $contact['name_first'] ?? null; ?>">
     </div>
     <div>
       <label for="name_last">Last Name</label>
-      <input type="text" id="name_last" name="name_last" placeholder="Last Name">
+      <input type="text" id="name_last" name="name_last" placeholder="Last Name"
+      value="<?php echo $contact['name_last'] ?? null; ?>">
     </div>
     <div>
       <label for="email">Email</label>
-      <input type="text" id="email" name="email" placeholder="Email">
+      <input type="text" id="email" name="email" placeholder="Email"
+      value="<?php echo $contact['email'] ?? null; ?>">
     </div>
     <div>
       <label for="birth_date">Birth Date</label>
-      <input type="date" id="birth_date" name="birth_date">
+      <input type="date" id="birth_date" name="birth_date"
+      value="<?php echo $contact['birth_date'] ?? null; ?>">
     </div>
+    <input type="hidden" name="id" value="<?php echo $id; ?>">
     <div>
       <button type="submit">Save</button>
       <a href="index.php">Cancel</a>
